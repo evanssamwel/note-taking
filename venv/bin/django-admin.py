1c183145377f0c0a98e9046ee980e3f32be5dd95
evanssamwel <nyamuevans@gmail.com>
2025-03-12 14:36:24 +0300

    added  venv environment

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bixerman/Downloads/Django-Note-Taking-App-Source-Code/Django Note Taking App Source Code/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
